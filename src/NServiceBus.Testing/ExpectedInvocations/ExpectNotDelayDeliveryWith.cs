e945981df61bc6700b154db6ac4831bba8bd176b
Tim Bussmann <tim.bussmann@particular.net>
2017-05-15 12:07:25 +0200

    fix ExpectDefer and ExpectNotDefer exception messages for DateTime based overloads

--- src/NServiceBus.Testing/ExpectedInvocations/ExpectNotDelayDeliveryWith.cs
+++ src/NServiceBus.Testing/ExpectedInvocations/ExpectNotDelayDeliveryWith.cs
@@ -19,7 +19,7 @@
 
             if (sentMessages.Any(s => check(s.Message, s.Options.GetDeliveryDelay().Value)))
             {
-                Fail($"Expected no message of type {typeof(TMessage).Name} to be deferred but a message matching your constraints was deferred.");
+                Fail($"Expected no message of type {typeof(TMessage).Name} to be deferred, but a message matching your constraints was deferred.");
             }
         }
 
