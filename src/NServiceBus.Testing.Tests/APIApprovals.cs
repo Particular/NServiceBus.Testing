65e59cfab22758bbcef60d7917de833cf32d9759
Brandon Ording <bording@gmail.com>
2017-09-15 18:37:42 -0400

    Add netcoreapp2.0 target to Tests project

--- src/NServiceBus.Testing.Tests/APIApprovals.cs
+++ src/NServiceBus.Testing.Tests/APIApprovals.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿#if NET452
+using System;
 using System.IO;
 using System.Linq;
 using System.Reflection;
@@ -31,3 +32,4 @@ public class APIApprovals
             );
     }
 }
+#endif
