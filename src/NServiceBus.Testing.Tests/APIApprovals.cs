58cde1017c16160c517410b791b62d27d6240711
Brandon Ording <bording@gmail.com>
2019-02-26 12:19:33 -0500

    Switch to Particular.Approvals

--- src/NServiceBus.Testing.Tests/APIApprovals.cs
+++ src/NServiceBus.Testing.Tests/APIApprovals.cs
@@ -1,35 +1,15 @@
-﻿#if NET452
-using System;
-using System.IO;
-using System.Linq;
-using System.Reflection;
-using System.Runtime.CompilerServices;
-using ApprovalTests;
+﻿using NServiceBus.Testing;
 using NUnit.Framework;
+using Particular.Approvals;
 using PublicApiGenerator;
 
 [TestFixture]
 public class APIApprovals
 {
     [Test]
-    [MethodImpl(MethodImplOptions.NoInlining)]
     public void Approve()
     {
-        var combine = Path.Combine(TestContext.CurrentContext.TestDirectory, "NServiceBus.Testing.dll");
-        var assembly = Assembly.LoadFile(combine);
-        var publicApi = Filter(ApiGenerator.GeneratePublicApi(assembly));
-        Approvals.Verify(publicApi);
-
-    }
-
-    string Filter(string text)
-    {
-        return string.Join(Environment.NewLine, text.Split(new[]
-        {
-            Environment.NewLine
-        }, StringSplitOptions.RemoveEmptyEntries)
-            .Where(l => !string.IsNullOrWhiteSpace(l))
-            );
+        var publicApi = ApiGenerator.GeneratePublicApi(typeof(Test).Assembly, excludeAttributes: new[] { "System.Runtime.Versioning.TargetFrameworkAttribute" });
+        Approver.Verify(publicApi);
     }
 }
-#endif
