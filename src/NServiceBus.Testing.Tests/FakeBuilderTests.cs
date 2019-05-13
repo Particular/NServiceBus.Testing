1306eb0055567d837043900eb796a6b0a8232db6
Andreas Öhlund <andreas.ohlund@particular.net>
2019-05-13 15:06:21 +0200

    Add fake builder tests

---
+++ src/NServiceBus.Testing.Tests/FakeBuilderTests.cs
@@ -0,0 +1,72 @@
+﻿namespace NServiceBus.Testing.Tests
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class FakeBuilderTests
+    {
+        [Test]
+        public void ShouldResolveRegisteredFuncs()
+        {
+            var builder = new FakeBuilder();
+
+            var instance = new SomeClass();
+
+            builder.Register(()=> instance);
+
+            Assert.AreSame(instance, builder.Build<SomeClass>());
+        }
+
+        [Test]
+        public void ShouldResolveRegisteredFuncArrays()
+        {
+            var builder = new FakeBuilder();
+
+            var instance = new SomeClass();
+            var instance2 = new SomeClass();
+
+            builder.Register(() => new []
+            {
+                instance,
+                instance2
+            });
+
+            CollectionAssert.Contains(builder.BuildAll<SomeClass>(), instance);
+            CollectionAssert.Contains(builder.BuildAll<SomeClass>(), instance2);
+        }
+
+        [Test]
+        public void ShouldResolveRegisteredObjectFuncs()
+        {
+            var builder = new FakeBuilder();
+
+            object instance = new SomeClass();
+
+            builder.Register(typeof(SomeClass),() => instance);
+
+            Assert.AreSame(instance, builder.Build<SomeClass>());
+        }
+
+        [Test]
+        public void ShouldResolveRegisteredObjectFuncArrays()
+        {
+            var builder = new FakeBuilder();
+
+            var instance = new SomeClass();
+            var instance2 = new SomeClass();
+
+            builder.Register(typeof(SomeClass), () => new object[]
+            {
+                instance,
+                instance2
+            });
+
+            CollectionAssert.Contains(builder.BuildAll<SomeClass>(), instance);
+            CollectionAssert.Contains(builder.BuildAll<SomeClass>(), instance2);
+        }
+
+        class SomeClass
+        {
+        }
+    }
+}
