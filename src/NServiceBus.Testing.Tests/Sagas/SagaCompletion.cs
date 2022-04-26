34cdf0c4fe65dd87be349e7792e5ed38a1510af8
Tim Bussmann <timbussmann@users.noreply.github.com>
2022-04-26 10:55:57 +0200

    Backport #384 to release-7.4
    Co-authored-by: David Boike <david.boike@gmail.com>

---
+++ src/NServiceBus.Testing.Tests/Sagas/SagaCompletion.cs
@@ -0,0 +1,43 @@
+﻿namespace NServiceBus.Testing.Tests.Sagas
+{
+    using System;
+    using System.Threading.Tasks;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class SagaCompletion
+    {
+        [Test]
+        public async Task MarkStartedSagaAsComplete()
+        {
+            var saga = new TestableSaga<TestSaga, TestSagaData>();
+
+            var result = await saga.Handle(new StartSagaMessage() { CorrelationId = Guid.NewGuid() });
+
+            Assert.IsTrue(result.Completed);
+        }
+
+        class TestSaga : NServiceBus.Saga<TestSagaData>, IAmStartedByMessages<StartSagaMessage>
+        {
+            protected override void ConfigureHowToFindSaga(SagaPropertyMapper<TestSagaData> mapper) => mapper
+                .MapSaga(d => d.CorrelationId)
+                .ToMessage<StartSagaMessage>(m => m.CorrelationId);
+
+            public Task Handle(StartSagaMessage message, IMessageHandlerContext context)
+            {
+                MarkAsComplete();
+                return Task.FromResult(0);
+            }
+        }
+
+        class TestSagaData : ContainSagaData
+        {
+            public Guid CorrelationId { get; set; }
+        }
+
+        class StartSagaMessage : IMessage
+        {
+            public Guid CorrelationId { get; set; }
+        }
+    }
+}
